104153f28ff015b90d29ab5483413df8de3217fa
ainul1997 <ainul4147@gmail.com>
2022-04-18 23:23:25 +0600

    added something

--- src/Pages/ServiseDetils/ServiseDetils.js
+++ src/Pages/ServiseDetils/ServiseDetils.js
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 const ServiseDetils = () => {
     const { serviseId } = useParams();
     return (
+
         <div>
             <h2> Theis is serviseDetils{serviseId}</h2>
         </div>
